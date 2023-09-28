766c6aa2536b505e59ec3c7a31ba15a12e20dd06
Lighttx26 <ncnhatquang26@gmail.com>
2023-09-28 14:36:28 +0700

    change dir

---
+++ server/server-simulate/src/Server.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.net.*;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        int port = 12345; // You can choose any available port
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("Server is listening on port " + port);
+
+        while (true) {
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Client connected: " + clientSocket.getInetAddress().getHostName());
+
+            // Create a thread to handle the client's request
+            Thread clientHandler = new ClientHandler(clientSocket);
+            clientHandler.start();
+        }
+    }
+}
+
+class ClientHandler extends Thread {
+    private final Socket clientSocket;
+
+    public ClientHandler(Socket socket) {
+        this.clientSocket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            // Input stream to receive data from the client
+            InputStream inputStream = clientSocket.getInputStream();
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+            DataInputStream dataInputStream = new DataInputStream(bufferedInputStream);
+
+            // Read the file name and size from the client
+            String fileName = dataInputStream.readUTF();
+            // long fileSize = dataInputStream.readLong();
+
+            System.out.println("Receiving file: " + fileName);
+
+            // Output stream to save the received file
+            FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+            byte[] buffer = new byte[1024];
+            int bytesRead;
+            long totalBytesReceived = 0;
+
+            while ((bytesRead = dataInputStream.read(buffer)) != -1) {
+                fileOutputStream.write(buffer, 0, bytesRead);
+                totalBytesReceived += bytesRead;
+            }
+
+            System.out.println("File received successfully. Size: " + totalBytesReceived + " bytes");
+
+            // Close streams and sockets
+            fileOutputStream.close();
+            dataInputStream.close();
+            clientSocket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
